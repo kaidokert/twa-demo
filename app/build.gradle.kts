f5a709709d98cf00a9bfe0919b200060c649702e
Kaido Kert <kaidokert@google.com>
2024-07-19 12:55:42 -0700

    Splash demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.mergenc.twademo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.mergenc.twademo"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
